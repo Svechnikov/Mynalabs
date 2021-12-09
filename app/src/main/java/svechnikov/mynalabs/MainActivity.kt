8c49e8fa12d80a88bcfbe4fd8e1df4a28b473375
Sergey Svechnikov <svechnikov66@gmail.com>
2021-12-09 16:18:21 +0500

    Create basic UI

--- app/src/main/java/svechnikov/mynalabs/MainActivity.kt
+++ app/src/main/java/svechnikov/mynalabs/MainActivity.kt
@@ -1,7 +1,11 @@
 package svechnikov.mynalabs
 
 import android.app.Activity
+import android.os.Bundle
 
 class MainActivity : Activity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
