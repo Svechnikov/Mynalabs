0c1d2c805c6abcd1cf20ea3a8fd48393c0e1a254
Sergey Svechnikov <svechnikov66@gmail.com>
2021-12-11 14:04:01 +0500

    Change blink parameters

--- app/src/main/java/svechnikov/mynalabs/LogoConfig.kt
+++ app/src/main/java/svechnikov/mynalabs/LogoConfig.kt
@@ -5,8 +5,8 @@ object LogoConfig {
     const val LOGO_RESOURCE = R.drawable.logo
 
     // How long it takes for one complete cycle of blinking to finish
-    const val BLINK_PERIOD = 5000f
+    const val BLINK_PERIOD = 1000f
 
     // Opacity change at extrema
-    const val BLINK_INTENSITY = 0.5f
+    const val BLINK_INTENSITY = 0.2f
 }
